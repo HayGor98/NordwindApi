f0597e492e767acc8866fde176fe4953a9b9c80d
Armen <hovhannisyan342@list.ru>
2019-09-22 21:52:16 +0400

    Test for Push

--- NordwindApi/Program.cs
+++ NordwindApi/Program.cs
@@ -20,5 +20,6 @@ namespace NordwindApi
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
     }
 }
